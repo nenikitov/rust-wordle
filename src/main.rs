ae72f97c7ca9d573ec2e3d9e2c20ccf2ada1a309
nenikitov <nenikitov@mail.ru>
2022-10-28 12:32:45 -0400

    Created a script to filter out invalid words from a word list

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use std::{fs::File, io::{BufReader, BufRead, self, BufWriter, Write}};
+
+const VOWELS: &str = "aeiouy";
+const CONSONANTS: &str = "bcdfghjklmnpqrstvwxz";
+
+
+
+fn main() {
+    let words = get_words("res/words_alpha.txt");
+    let words: Vec<String> = words
+        .into_iter()
+        .filter(is_a_valid_word)
+        .collect();
+    write_words("res/words_filtered.txt", &words);
+}
+
+fn write_words(file_name: &str, words: &Vec<String>) {
+    let file = File::create(file_name).expect("File not found");
+    let mut writer = BufWriter::new(file);
+    writer.write_all(words.join("\n").as_bytes()).expect("File could not be written");
+}
+
+fn get_words(file_name: &str) -> Vec<String> {
+    let file = File::open(file_name).expect("File not found");
+    let reader = BufReader::new(file);
+    reader.lines()
+        .filter_map(io::Result::ok)
+        .collect()
+}
+
+fn is_a_valid_word(word: &String) -> bool {
+    let len = word.len();
+    if (4..8).contains(&len) {
+        let consecutive_vowels = count_consecutive(word, VOWELS);
+        let consecutive_consonants = count_consecutive(word, CONSONANTS);
+        let percent_vowels = count_all(word, VOWELS) as f64 / len as f64;
+        let percent_consonants = count_all(word, CONSONANTS) as f64 / len as f64;
+
+        consecutive_consonants <= 4
+            && consecutive_vowels <= 2
+            && percent_vowels <= 0.9
+            && percent_consonants <= 0.9
+    }
+    else {
+        false
+    }
+}
+
+
+fn count_all(word: &String, set: &str) -> usize {
+    word.chars().filter(|char| {
+        set.contains(*char)
+    }).count()
+}
+
+fn count_consecutive(word: &String, set: &str) -> usize {
+    let mut max: usize = 0;
+    let mut current: usize = 0;
+
+    for char in word.chars() {
+        if set.contains(char) {
+            current += 1;
+        }
+        else if current > max {
+            max = current;
+            current = 0;
+        }
+    }
+    max
+}
